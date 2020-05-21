cf6ebd62e05cfb55cd80384ff9cb93df524e9492
Fit <phitas24@gmail.com>
2020-05-21 21:04:38 +0300

    наш 1 комит

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+console.log(4/0);
+console.log('string'*9);
+
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plump.pmg', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[2])
+
+// alert("hello worls");
+
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+let answer = prompt("Are you 18?", "да");
+console.log(answer);
+
+
+
+
+
+
---
+++ script1.js
@@ -0,0 +1,49 @@
+// 'str', "str", `str`;
+// true / false
+// let a;
+// console.log(a);
+
+// const obj = {
+//     name: 'Alex'
+// };
+
+// const arr = [1, 2, 3, 4];
+
+// to String
+
+// 1)
+
+// console.log(typeof(String(4)));
+
+// // 2)
+// console.log(typeof("eee" + 5));
+// console.log('https://vk.com/catalog/' + 5);
+
+// // to string
+// // 1)
+// console.log(typeof(Number('4')));
+// // 2)
+// console.log(typeof(+'5'));
+// // 3)
+// console.log(typeof(parseInt('15px', 10)));
+
+// let ans = +prompt("Hello", '');
+
+// // 0, '', null, undefined, NaN
+
+// let switcher = null;
+
+// if (switcher) {
+//     console.log("Working..")
+// }
+
+// switcher = 1;
+
+// if (switcher) {
+//     console.log("Working..")
+// }
+
+// // 2)
+// console.log(typeof(Boolean('4')));
+// // 3)
+// console.log(typeof(!!'4'));
